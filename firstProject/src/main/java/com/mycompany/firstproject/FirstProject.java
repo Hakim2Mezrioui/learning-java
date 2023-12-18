502517d8979b1fd2ae8ace3872b55236888b50be
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2023-12-18 20:42:03 +0100

    comments

---
+++ firstProject/src/main/java/com/mycompany/firstproject/FirstProject.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author H-black
+ */
+
+// line comment
+
+/*
+    a lot of lines comment
+*/
+
+public class FirstProject {
+    
+    public static void main(String[] args) {
+        // tab
+        System.out.print("word1\tword2");
+        
+        // backspace
+        System.out.println("mezrioui hakim\b");
+        
+        // new line
+        System.out.println("line1\nline2");
+        
+        // single quote && double quote
+        System.out.println("double quote \"");
+        System.out.println("single quote \'");
+        
+        // backslash
+        System.out.println("backslash \\");
+           
+        // \r like \n actuallement
+        System.out.println("hello hakim \r welcome");
+        
+        // formfeed
+        System.out.println("form feed \f");
+    }
+}
