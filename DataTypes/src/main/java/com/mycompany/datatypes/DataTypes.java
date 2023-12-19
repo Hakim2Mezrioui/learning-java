6dc6fc5525563f8b3edbc3f1660733f33f741876
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2023-12-19 13:45:07 +0100

    first commit

--- DataTypes/src/main/java/com/mycompany/datatypes/DataTypes.java
+++ DataTypes/src/main/java/com/mycompany/datatypes/DataTypes.java
@@ -11,6 +11,34 @@ package com.mycompany.datatypes;
 public class DataTypes {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Byte.MAX_VALUE);
+        byte byteNumber = -128;  // -128 à 127
+        System.out.println(byteNumber);
+        
+        System.out.println(Short.MAX_VALUE);
+        short shortNumber = 32588; // -32768 à 32767
+        System.out.println(shortNumber);
+        
+        System.out.println(Integer.MAX_VALUE);
+        int intergerNumber = 2147483647;
+        System.out.println(intergerNumber);
+        
+        System.out.println(Long.MAX_VALUE);
+        long longNumber = 9223372036854775807L;
+        System.out.println(longNumber);
+        
+        System.out.println(Float.MAX_VALUE);
+        float floatNumber = 3.4028235E38f;
+        System.out.println(floatNumber);
+        
+        System.out.println(Double.MAX_VALUE);
+        double doubleNumber = 1.7976931348623157E308;
+        System.out.println(doubleNumber);
+        
+        char caractere = 'h';
+        System.out.println(caractere);
+        
+        String myName = "mezrioui hakim";
+        System.out.println(myName);
     }
 }
---
+++ arithmeticOperators/src/main/java/com/mycompany/arithmeticoperators/ArithmeticOperators.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arithmeticoperators;
+
+/**
+ *
+ * @author H-black
+ */
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        // basic operators (arithmetic Operators)
+        byte x = 10;
+        System.out.println("x = " + x);
+        
+        System.out.println((5+6)*2);
+        int exp = (5+6)*2;
+        System.out.println(exp);
+        
+        long res = (58393 * 2)/4;
+        System.out.println(res);
+        
+        int resultat = 13/3;
+        int reste = 13%3;
+        
+        System.out.println("13 divise par 3 vaut " + resultat + " le reste est " + reste);
+        
+        System.out.println(15.3f/(float)3);
+        
+        float num = 5.3f;
+        System.out.println("num " + (int)num);
+    }
+}
