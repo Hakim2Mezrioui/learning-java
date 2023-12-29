ce9a133d27265c13adc6362abf526487c765be33
mezrioui hakim <mezrioui.hakim@gmail.com>
2023-12-29 07:19:33 +0100

    method part 1

--- method_function_part1/src/main/java/com/mycompany/method_function_part1/Method_function_part1.java
+++ method_function_part1/src/main/java/com/mycompany/method_function_part1/Method_function_part1.java
@@ -18,13 +18,31 @@ public class Method_function_part1 {
     static int sumTowNumbers(int n1, int n2) {
         int sum = n1 + n2;
         return sum;
+
+        return n1 + n2;
+    }
+    
+    static void sumTowNumbers2(int n1, int n2) {
+        if (n1 == 0 && n2 == 0) 
+            return;
+        System.out.println("sum = " + (n1 + n2));
     }
 
     public static void main(String[] args) {
         greeting();
         greeting();
         
+        System.out.println("------------------------------");
+//        string salution = greeting(); // error because the greeting function return void
+        
+        System.out.println("------------------------------");
+        
         int res = sumTowNumbers(3, 5);
         System.out.println("sum = " + res);
+        System.out.println("sum = " + sumTowNumbers(2, 3));
+        
+        System.out.println("------------------------------");
+
+        sumTowNumbers2(0, 0);
     }
 }
