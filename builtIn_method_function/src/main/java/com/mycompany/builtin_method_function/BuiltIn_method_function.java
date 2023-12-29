998e28acaba29ff895b53999bda8607f9bd7c35b
mezrioui hakim <mezrioui.hakim@gmail.com>
2023-12-29 08:15:03 +0100

    create the built in method functin file

---
+++ builtIn_method_function/src/main/java/com/mycompany/builtin_method_function/BuiltIn_method_function.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.builtin_method_function;
+
+/**
+ *
+ * @author Mezrioui_Hakim
+ */
+public class BuiltIn_method_function {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
